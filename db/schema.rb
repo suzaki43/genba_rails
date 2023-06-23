47c445e81be70e3b93c7189ff4f3d0a870103966
suzaki43 <kazuki.s0818@gmail.com>
2023-06-23 10:53:58 +0900

    4-3

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_17_080054) do
+ActiveRecord::Schema.define(version: 2023_04_25_010310) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
